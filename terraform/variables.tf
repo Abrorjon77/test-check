f2024f32a2d05d21cc943b0813190699da56afa4
abror <abroor@yahoo.com>
2025-09-30 10:13:47 -0400

    add s

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,5 +3,5 @@ variable "aws_region" {
 }
 
 variable "key_name" {
-  description = "Name of the AWS key pair to use for the instance."
+  description = "Name of the AWS key pair to use for the instances."
 }
